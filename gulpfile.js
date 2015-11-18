048cc80e9e48edfb040c49e4861abb415b42c9da
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-11-18 10:10:47 -0700

    Added task to compile jade page to html.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,18 @@
  * Created by cfiloteo on 11/17/15.
  */
 var gulp = require('gulp'),
-	rename = require('gulp-rename');
+	rename = require('gulp-rename'),
+	jade   = require('gulp-jade');
 
 gulp.task('normalize', function() {
 	return gulp.src(['node_modules/normalize.css/normalize.css'])
 		.pipe(rename('_normalize.scss'))
 		.pipe(gulp.dest('src/'));
+});
+gulp.task('make-index', function() {
+	return gulp.src('./views/index.jade')
+		.pipe(jade({
+			pretty: true
+		}))
+		.pipe(gulp.dest('./'))
 });
