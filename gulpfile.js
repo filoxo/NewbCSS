ee6c2de70054d2944d9b00ec54562f630782dec8
Carlos Filoteo <carlos_filoteo@byu.edu>
2015-12-20 20:41:53 -0700

    + Babel to transpile ES6 js

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,24 @@
  */
 var gulp = require('gulp'),
 	rename = require('gulp-rename'),
-	jade   = require('gulp-jade');
+	jade   = require('gulp-jade'),
+	babel  = require("gulp-babel");
 
-gulp.task('normalize', function() {
+gulp
+	.task('normalize', function() {
 	return gulp.src(['node_modules/normalize.css/normalize.css'])
 		.pipe(rename('_normalize.scss'))
-		.pipe(gulp.dest('src/'));
-});
-gulp.task('make-index', function() {
-	return gulp.src('./views/index.jade')
-		.pipe(jade({
-			pretty: true
-		}))
-		.pipe(gulp.dest('./'))
-});
+		.pipe(gulp.dest('src/scss/'))
+	})
+	.task('transform', function () {
+		return gulp.src('src/js/*.js')
+			.pipe(babel())
+			.pipe(gulp.dest('dist/js/'));
+	})
+	.task('make-index', function() {
+		return gulp.src('./views/index.jade')
+			.pipe(jade({
+				pretty: true
+			}))
+			.pipe(gulp.dest('./'))
+	});
