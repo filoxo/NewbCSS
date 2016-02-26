736aacb4f905a9ea55e852e5ebd3b24471d34b77
Carlos Filoteo <carlos_filoteo@byu.edu>
2016-02-26 10:00:36 -0700

    Update index to notify of successful run.

--- index.js
+++ index.js
@@ -2,19 +2,15 @@
  * Created by cfiloteo on 11/17/15.
  */
 var express = require('express'),
-		app = express(),
-		routes = express.Router();
-
+	app 	= express(),
+	routes	= express.Router();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/'));
-
 routes.get('/', function(req, res){
-	console.log('render / with index.jade');
 	res.render('index');
 });
-
 app.use('/test', routes);
 app.listen(3000);
-
+console.log('App running on localhost:3000')
 module.exports = app;
