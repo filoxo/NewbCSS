e3fe25dc50f8dac8bb9d8ce2791872cbf831d011
Carlos Filoteo <carlos_filoteo@byu.edu>
2016-02-26 10:00:02 -0700

    Add sass and sass:watch tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 /**
  * Created by cfiloteo on 11/17/15.
  */
-var gulp = require('gulp'),
-	rename = require('gulp-rename'),
-	jade   = require('gulp-jade'),
-	babel  = require("gulp-babel");
+var gulp 	= require('gulp'),
+	rename	= require('gulp-rename'),
+	jade	= require('gulp-jade'),
+	babel	= require("gulp-babel"),
+	sass 	= require('gulp-sass');;
 
 gulp
+	.task('sass', function() {
+		gulp.src('src/scss/*.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('dist/css/'))
+	})
+	.task('default',function() {
+	    gulp.watch('src/scss/*.scss',['sass']);
+	    gulp.watch('views/index.jade', ['make-index']);
+	})
 	.task('normalize', function() {
-	return gulp.src(['node_modules/normalize.css/normalize.css'])
-		.pipe(rename('_normalize.scss'))
-		.pipe(gulp.dest('src/scss/'))
+		return gulp.src(['node_modules/normalize.css/normalize.css'])
+			.pipe(rename('_normalize.scss'))
+			.pipe(gulp.dest('src/scss/'))
 	})
 	.task('transform', function () {
 		return gulp.src('src/js/*.js')
@@ -18,7 +28,7 @@ gulp
 			.pipe(gulp.dest('dist/js/'));
 	})
 	.task('make-index', function() {
-		return gulp.src('./views/index.jade')
+		gulp.src('./views/index.jade')
 			.pipe(jade({
 				pretty: true
 			}))
